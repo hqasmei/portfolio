cdf9389ac0bc2ed553b1ffd1a3c37cea7809ce5e
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-12-16 23:30:18 -0800

    Updated header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,48 +5,39 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import DialogNavMenu from '@/components/dialog-nav-menu';
 import { CONFIG } from '@/lib/config';
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <header className="flex flex-row justify-between items-center space-y-0 w-full max-w-3xl animate-slide-from-down-and-fade-1 md:px-6">
-        <div className="flex flex-row items-center space-x-3">
-          <Image
-            src={CONFIG.headshot}
-            alt="Hosna Qasmei headshot"
-            width={100}
-            height={100}
-            className="rounded-full h-[48px] w-[48px]"
-            priority
-          />
-          <div className="flex flex-col">
-            <span className="font-semibold">{CONFIG.name}</span>
-            <span className="text-neutral-400">{CONFIG.title}</span>
-          </div>
+    <header className="flex flex-row justify-between items-center space-y-0 w-full max-w-3xl animate-slide-from-down-and-fade-1 md:px-6">
+      <div className="flex flex-row items-center space-x-3">
+        <Image
+          src={CONFIG.headshot}
+          alt="Hosna Qasmei headshot"
+          width={100}
+          height={100}
+          className="rounded-full h-[48px] w-[48px]"
+          priority
+        />
+        <div className="flex flex-col">
+          <span className="font-semibold">{CONFIG.name}</span>
+          <span className="text-neutral-400">{CONFIG.title}</span>
         </div>
-      </header>
-      <DialogNavMenu isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className="flex flex-col gap-4 items-start">
-          {CONFIG.socials.map((social, idx) => {
-            return (
-              <Link
-                onClick={() => setIsOpen(false)}
-                href={social.link}
-                target="_blank"
-                key={idx}
-              >
-                <div className="flex items-center gap-2 text-sm text-neutral-400 hover:text-neutral-200 group duration-200">
-                  {social.icon}
-                  <span>{social.platform}</span>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </DialogNavMenu>
-    </>
+      </div>
+      <div className="flex flex-row items-center gap-4">
+        <Link
+          href="/projects"
+          className="text-neutral-400 hover:text-secondary duration-200 transition-all"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/posts"
+          className="text-neutral-400 hover:text-secondary duration-200 transition-all"
+        >
+          Posts
+        </Link>
+      </div>
+    </header>
   );
 }
