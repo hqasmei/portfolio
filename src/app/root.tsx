b4f4cd2b8c426f03a9934d1039172add7a710401
Hosna Qasmei <hosna.qasmei@gmail.com>
2025-06-04 14:19:22 -0700

    added script to format and lint through cli

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -44,7 +44,7 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <html lang="en" >
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
